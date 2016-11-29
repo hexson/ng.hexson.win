f6c43a9dad2692755fb2654c010940e10ba0e8c9
hadley <1477546184@qq.com>
2016-11-29 21:17:51 +0800

    Update all module

--- src/app/containers/all/all.component.ts
+++ src/app/containers/all/all.component.ts
@@ -10,27 +10,40 @@ import { AppService } from '../../app.service';
 })
 
 export class AllComponent implements OnInit, AfterViewInit {
-  issue: any[];
+  issues: any[];
   loadingView = true;
   errorView = false;
+  loadBeforeText = '加载更早的文章';
+  btnClass = 'load-before block f18 none';
 
   constructor(
     private appService: AppService
   ) {}
 
+  loadBefore(): void {
+    if (this.loadBeforeText == '加载更早的文章'){
+      this.loadBeforeText = '没有更多了';
+    }else if (this.btnClass.indexOf('load-before-animation') < 0){
+      this.btnClass = 'load-before block f18 load-before-animation';
+      setTimeout(() => this.btnClass = 'load-before block f18', 400);
+    }
+  }
+
   ngOnInit(): void {
     NProgress.start();
-    if (!this.appService.issue){
-      this.appService.getIssue()
+    if (!this.appService.issues){
+      this.appService.getIssues()
       .then(data => {
-        this.issue = data;
+        this.issues = data;
         this.loadingView = false;
+        this.btnClass = 'load-before block f18';
       })
       .catch(msg => this.errorView = true);
     }
     else {
-      this.issue = this.appService.issue;
+      this.issues = this.appService.issues;
       this.loadingView = false;
+      this.btnClass = 'load-before block f18';
     }
   }
 
