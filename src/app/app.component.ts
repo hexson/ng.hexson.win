5f9646ab0b22bc1d3ff9b55add5b12889c545c6d
hadley <1477546184@qq.com>
2016-12-18 20:12:14 +0800

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,18 @@ import { BASE } from './constants/base';
 })
 
 export class AppComponent implements OnInit {
+  baiduStatistics() {
+    if (window.location.host.indexOf('hexson') >= 0){
+      var _hmt: any[] = _hmt || [];
+      (function(){
+        var hm = document.createElement('script');
+        var s = document.getElementsByTagName('script')[0];
+        hm.src = '//hm.baidu.com/hm.js?' + BASE['baidu'];
+        s.parentNode.insertBefore(hm, s);
+      })();
+    }
+  }
+
   duoshuoComments() {
     window['duoshuoQuery'] = { short_name: BASE['duoshuo'] };
     (function(){
@@ -25,6 +37,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     NProgress.configure({ showSpinner: false });
+    this.baiduStatistics();
     this.duoshuoComments();
     console.log('%cWelcome to Hexson\'s blog.\n欢迎来到我的博客,商务合作及友链等请联系:\nE-mail - 1477546184@qq.com', 'color: #1d89e2; font-size: 13px; line-height: 20px;');
   }
